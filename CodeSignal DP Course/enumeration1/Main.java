ae17884d56af5c6f306fda84080fdf49af07ac36
walid <walidrahman.officials@gmail.com>
2026-03-12 14:36:39 +0600

    added

---
+++ CodeSignal DP Course/enumeration1/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Planet mars = Planet.MARS;
+
+        System.out.println(mars.getMass());
+        System.out.println(mars.getRadius());
+        System.out.println(mars.surfaceGravity());
+
+    }
+}
