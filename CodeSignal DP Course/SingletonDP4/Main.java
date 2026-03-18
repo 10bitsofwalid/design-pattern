69ec86fe7d283c4fccde9612f4dbb7d4f3b89803
walid <walidrahman.officials@gmail.com>
2026-03-18 11:45:19 +0600

    added

---
+++ CodeSignal DP Course/SingletonDP4/Main.java
@@ -0,0 +1,9 @@
+package SingletonDP4;
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO: Get the Singleton instance using getInstance() method
+        Singleton singleton = Singleton.getInstance();
+        singleton.showMessage();
+    }
+}
---
+++ CodeSignal DP Course/SingletonDP4/Singleton.java
@@ -0,0 +1,23 @@
+package SingletonDP4;
+
+public class Singleton {
+
+    // TODO: Create a private constructor to prevent instantiation from other classes
+    private Singleton(){}
+
+    private static class SingletonHelper {
+        // TODO: Initialize the Singleton instance when this class is loaded
+        private static final Singleton INSTANCE = new Singleton();
+    }
+
+    // TODO: Create a public method to provide access to the Singleton instance
+    public static Singleton getInstance(){
+        return SingletonHelper.INSTANCE;
+    }
+
+
+    // Example method for demonstration purposes
+    public void showMessage() {
+        System.out.println("Hello from the Singleton instance!");
+    }
+}
