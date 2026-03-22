05233a6c0347818862e2c6d7331032e9fca65b10
walid <walidrahman.officials@gmail.com>
2026-03-22 22:10:28 +0600

    added

---
+++ CodeSignal DP Course/BuilderDP1/Main.java
@@ -0,0 +1,18 @@
+package BuilderDP1;
+
+public class Main {
+    public static void main(String[] args) {
+        // Building a House object using the Builder pattern
+        House house = new House.HouseBuilder(4, 3)  // Required parameters
+                .setGarage(true)                    // Optional parameter
+                .setSwimmingPool(true)              // Optional parameter
+                .setGarden(true)                    // Optional parameter
+                .build();
+
+        System.out.println("House has " + house.getRooms() + " rooms.");
+        System.out.println("House has " + house.getBathrooms() + " bathrooms.");
+        System.out.println("House has a garage: " + house.hasGarage());
+        System.out.println("House has a swimming pool: " + house.hasSwimmingPool());
+        System.out.println("House has a garden: " + house.hasGarden());
+    }
+}
